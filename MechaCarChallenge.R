ddadaee4d52addb44c130c2d3df4143d643939df
J0HN-MAC <johnmac1827@gmail.com>
2023-02-01 20:24:39 -0500

    modified  RScript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,17 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 # Summarize to get p-value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD ,data=MechaCar_data))
+
+
+####### Deliverable 2 #######
+
+# import and read in the Suspension_Coil.csv file as a table
+suspension_coil_table <- read.csv(file='Data/Suspension_Coil.csv', check.names=F,
+                                  stringsAsFactors=F)
+
+# Create dataframe and get summary statistics
+total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Create a lot summary dataframe using group_by() and summarize()
+lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize
+(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
